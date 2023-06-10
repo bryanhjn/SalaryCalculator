06648f9defb74073067bcb53b1bbe2369e20046e
bryanhjn <bryanhjn@outlook.com>
2023-06-11 00:46:00 +0800

    mod project info

--- backEnd/src/main/java/org/bryanhjn/ldsalarycalculator/LD_SalaryCalculatorApplication.java
+++ backEnd/src/main/java/org/bryanhjn/ldsalarycalculator/LD_SalaryCalculatorApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package org.bryanhjn.ldsalarycalculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class LD_SalaryCalculatorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(LD_SalaryCalculatorApplication.class, args);
 	}
 
 }
